ca352fdacc165ed842d40258e80fe8b88c1bd299
marcogdepinto <marcog.depinto@gmail.com>
2019-08-12 18:33:13 +0200

    Now javascript file is correctly linked to the template

--- App/static/javascript/javascript.js
+++ App/static/javascript/javascript.js
@@ -1,7 +1,12 @@
-async function saveFile()
+async function loadFile() // TODO: fix
 {
     let formData = new FormData();
     formData.append("file", file.files[0]);
     await fetch('http://127.0.0.1:8000/file/upload/', {method: "POST", body: formData});
     alert("Data Uploaded: ");
+}
+
+function makePredictions()
+{
+    console.log("Not yet implemented");
 }
