e2c81917be9abb5ff077526019c71891f940d3c2
marcogdepinto <marcog.depinto@gmail.com>
2019-08-16 09:44:37 +0200

    Updated urls.py

--- DjangoRestDeepLearning/settings.py
+++ DjangoRestDeepLearning/settings.py
@@ -195,6 +195,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 INTERNAL_IPS = [
     '127.0.0.1',
--- DjangoRestDeepLearning/urls.py
+++ DjangoRestDeepLearning/urls.py
@@ -25,13 +25,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
-
     ] + urlpatterns
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
