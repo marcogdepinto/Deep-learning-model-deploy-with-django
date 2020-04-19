d30c8a07894faeea7ad3b48167f9d50bf1b5689b
Marco Giuseppe de Pinto <marcog.depinto@gmail.com>
2020-04-19 16:43:55 +0200

    Update settings.py

--- DjangoRestDeepLearning/settings.py
+++ DjangoRestDeepLearning/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import django_heroku
 import os
+
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
