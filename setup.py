d3c4e2c6472fbb58b8f91a39edfc7a43bc37284a
Marco Giuseppe de Pinto <marcog.depinto@gmail.com>
2020-04-19 15:02:40 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Django-Emotion-Classification-Ravdess-API',
+    version='1.1.1',
+    packages=['App', 'DjangoRestDeepLearning'],
+    url='https://github.com/marcogdepinto/Deep-learning-model-deploy-with-django',
+    license='',
+    author='Marco Giuseppe de Pinto',
+    author_email='marcog.depinto@gmail.com',
+    description='Serving a keras model (neural networks) in a website with the python Django-REST framework.'
+)
